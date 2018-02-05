0383f8171e2c66da9b003b13e50903a9d1e39016
JeromeWang <mulituzhen@gmail.com>
2018-02-05 17:23:40 +0800

    client auth

---
+++ Zookeeper Form_files/validateComments.js
@@ -0,0 +1,7 @@
+function validateComments(input) {
+    if (input.value.length < 20) {
+        input.setCustomValidity("You need to comment in more detail.")
+    }else {
+        input.setCustomValidity("");
+    }
+}
