6910914c2c867f18c4c65e855653329d8fdbb7c9
JeromeWang <mulituzhen@gmail.com>
2018-02-17 19:22:10 +0800

    cut picture

--- drawPicture.js
+++ drawPicture.js
@@ -4,7 +4,10 @@ var context = canvas.getContext("2d");
 var img = new Image();
 //在图像加载完成后绘图
 img.onload = function () {
-    context.drawImage(img,0,0);
+    //将图像缩放
+    context.drawImage(img,0,0,300,200);
+    //在原图片的（0，0）处截取800*787以200*150大小呈现在（500，0）
+    context.drawImage(img,0,0,800,787,500,0,200,150);
 }
 //加载图片
 img.src = "drawPicture.jpg";
