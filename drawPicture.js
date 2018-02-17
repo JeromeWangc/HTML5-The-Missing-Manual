4e34d5315feaf5a393e0f41ab2ec01435e5ab9a7
JeromeWang <mulituzhen@gmail.com>
2018-02-17 19:34:57 +0800

    fill text

--- drawPicture.js
+++ drawPicture.js
@@ -1,5 +1,8 @@
 var canvas = document.getElementById("drawPicture");
 var context = canvas.getContext("2d");
+//粗体 大小 字体
+context.font = "bold 20px Arial";
+
 //新建图像对象
 var img = new Image();
 //在图像加载完成后绘图
@@ -8,6 +11,12 @@ img.onload = function () {
     context.drawImage(img,0,0,300,200);
     //在原图片的（0，0）处截取800*787以200*150大小呈现在（500，0）
     context.drawImage(img,0,0,800,787,500,0,200,150);
+    //文本参照行
+    context.textBaseline = "top";
+    //文本填充颜色
+    context.fillStyle = "black";
+    //填充文本
+    context.fillText("I'm stuck in a canvas. Someone let me out!",0,250)
 }
 //加载图片
 img.src = "drawPicture.jpg";
