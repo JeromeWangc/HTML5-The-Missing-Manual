92076db336831eb51a2873f61492020adee179a7
JeromeWang <mulituzhen@gmail.com>
2018-02-17 16:01:30 +0800

    test translate

--- testCanvas.js
+++ testCanvas.js
@@ -88,4 +88,18 @@ function drawBezier(){
     context.stroke();
 }
 
-window.onload = drawBezier;
+function testTranslate(){
+    var canvas = document.getElementById("drawingCanvas");
+    var context = canvas.getContext("2d");
+
+    context.rect(0,0,30,30);
+
+    context.translate(50,50);
+    context.rect(0,0,30,30);
+
+    context.translate(50,50);
+    context.rect(0,0,30,30);
+
+    context.stroke();
+}
+window.onload = testTranslate;
